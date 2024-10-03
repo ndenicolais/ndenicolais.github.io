ca9a64d743cdd15b2e831d903e0ee5c0dbe2f04f
ndenicolais <ndn21dev@gmail.com>
2024-10-03 10:59:58 +0200

    Updated globals

--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class GlobalKeys {
   static final GlobalKey topSectionKey = GlobalKey();
-  static final GlobalKey aboutSectionKey = GlobalKey();
+  static final GlobalKey bioSectionKey = GlobalKey();
   static final GlobalKey skillsSectionKey = GlobalKey();
   static final GlobalKey worksSectionKey = GlobalKey();
   static final GlobalKey projectsSectionKey = GlobalKey();
