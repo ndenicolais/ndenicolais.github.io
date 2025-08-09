8ff2176915d0430dd7e12e56f637a23368489f2e
ndenicolais <ndn21dev@gmail.com>
2025-08-09 18:50:42 +0200

    Refactor MyDrawer to improve layout, enhance organization, and integrate ThemeController for theme management

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ming_cute_icons/ming_cute_icons.dart';
-import 'package:myportfolio/theme/theme_notifier.dart';
+import 'package:myportfolio/theme/theme_controller.dart';
 import 'package:myportfolio/utils/constants.dart';
 import 'package:myportfolio/utils/globals.dart';
 import 'package:myportfolio/utils/strings.dart';
-import 'package:provider/provider.dart';
+import 'package:myportfolio/utils/style.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MyDrawer extends StatelessWidget {
   final Function(GlobalKey) onScrollToSection;
 
-  const MyDrawer({
-    Key? key,
-    required this.onScrollToSection,
-  }) : super(key: key);
+  const MyDrawer({super.key, required this.onScrollToSection});
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    final styles = Style.getStyleParams(screenWidth);
+
     return Drawer(
-      width: 260,
+      width: 240,
       child: Container(
         decoration: BoxDecoration(
           color: Theme.of(context).colorScheme.secondary,
@@ -27,26 +28,13 @@ class MyDrawer extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            const SizedBox(height: 60),
-            Container(
-              width: 120,
-              height: 120,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Theme.of(context).colorScheme.tertiary,
-                  width: 2.0,
-                ),
-              ),
-              child: ClipOval(
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  width: 80,
-                  height: 80,
-                ),
-              ),
+            const SizedBox(height: 10),
+            Image.asset(
+              'assets/images/logo.png',
+              width: 140,
+              height: 140,
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 10),
             Divider(
               thickness: 1,
               indent: 40,
@@ -54,115 +42,91 @@ class MyDrawer extends StatelessWidget {
               color: Theme.of(context).colorScheme.primary,
             ),
             const SizedBox(height: 10),
-            ListTile(
-              leading: Icon(
-                size: 24,
-                MingCuteIcons.mgc_arrows_up_fill,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              title: Text(
-                dTop,
-                style: GoogleFonts.montserrat(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontSize: 14,
-                ),
-              ),
+            _drawerTile(
+              context: context,
+              icon: MingCuteIcons.mgc_user_4_line,
+              title: drawerAboutMe,
               onTap: () {
-                onScrollToSection(GlobalKeys.topSectionKey);
+                onScrollToSection(GlobalKeys.aboutMeSectionKey);
                 Navigator.of(context).pop();
               },
+              styles: styles,
             ),
-            ListTile(
-              leading: Icon(
-                size: 24,
-                MingCuteIcons.mgc_user_4_fill,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              title: Text(
-                dBio,
-                style: GoogleFonts.montserrat(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontSize: 14,
-                ),
-              ),
+            _drawerTile(
+              context: context,
+              icon: MingCuteIcons.mgc_briefcase_line,
+              title: drawerExperiences,
               onTap: () {
-                onScrollToSection(GlobalKeys.bioSectionKey);
+                onScrollToSection(GlobalKeys.experiencesSectionKey);
                 Navigator.of(context).pop();
               },
+              styles: styles,
             ),
-            ListTile(
-              leading: Icon(
-                size: 24,
-                MingCuteIcons.mgc_briefcase_fill,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              title: Text(
-                dWorks,
-                style: GoogleFonts.montserrat(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontSize: 14,
-                ),
-              ),
-              onTap: () {
-                onScrollToSection(GlobalKeys.worksSectionKey);
-                Navigator.of(context).pop();
-              },
-            ),
-            ListTile(
-              leading: Icon(
-                size: 24,
-                MingCuteIcons.mgc_code_fill,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              title: Text(
-                dProjects,
-                style: GoogleFonts.montserrat(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontSize: 14,
-                ),
-              ),
+            _drawerTile(
+              context: context,
+              icon: MingCuteIcons.mgc_code_line,
+              title: drawerProjects,
               onTap: () {
                 onScrollToSection(GlobalKeys.projectsSectionKey);
                 Navigator.of(context).pop();
               },
+              styles: styles,
             ),
-            ListTile(
-              leading: Icon(
-                size: 24,
-                MingCuteIcons.mgc_palette_fill,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              title: Text(
-                dTheme,
-                style: GoogleFonts.montserrat(
-                  color: Theme.of(context).colorScheme.primary,
-                  fontSize: 14,
-                ),
-              ),
-              trailing: GestureDetector(
-                onTap: () {
-                  Provider.of<ThemeNotifier>(context, listen: false)
-                      .switchTheme();
-                },
-                child: Switch(
-                  value: Provider.of<ThemeNotifier>(context).isDarkMode,
-                  onChanged: (value) {
-                    Provider.of<ThemeNotifier>(context, listen: false)
-                        .switchTheme();
+            _drawerTile(
+              context: context,
+              icon: MingCuteIcons.mgc_backpack_line,
+              title: drawerEducation,
+              onTap: () {
+                onScrollToSection(GlobalKeys.educationSectionKey);
+                Navigator.of(context).pop();
+              },
+              styles: styles,
+            ),
+            _drawerTile(
+              context: context,
+              icon: MingCuteIcons.mgc_user_info_line,
+              title: drawerContacts,
+              onTap: () {
+                onScrollToSection(GlobalKeys.contactsSectionKey);
+                Navigator.of(context).pop();
+              },
+              styles: styles,
+            ),
+            _drawerTile(
+              context: context,
+              icon: MingCuteIcons.mgc_palette_line,
+              title: drawerTheme,
+              onTap: () {},
+              styles: styles,
+              trailing: Transform.scale(
+                scale: 0.8,
+                child: Obx(
+                  () {
+                    final themeController = Get.find<ThemeController>();
+                    return Switch(
+                      value: themeController.isDark,
+                      onChanged: (value) {
+                        themeController.switchTheme();
+                      },
+                      activeColor: Theme.of(context).colorScheme.primary,
+                      activeTrackColor: Theme.of(context)
+                          .colorScheme
+                          .primary
+                          .withValues(alpha: 0.5),
+                      inactiveThumbColor:
+                          Theme.of(context).colorScheme.secondary,
+                      inactiveTrackColor: Theme.of(context).colorScheme.primary,
+                      trackOutlineColor:
+                          WidgetStateProperty.resolveWith<Color?>(
+                        (Set<WidgetState> states) {
+                          if (states.contains(WidgetState.selected)) {
+                            return Theme.of(context).colorScheme.onTertiary;
+                          }
+                          return Theme.of(context).colorScheme.onTertiary;
+                        },
+                      ),
+                    );
                   },
-                  activeColor: Theme.of(context).colorScheme.primary,
-                  activeTrackColor:
-                      Theme.of(context).colorScheme.primary.withOpacity(0.5),
-                  inactiveThumbColor: Theme.of(context).colorScheme.secondary,
-                  inactiveTrackColor: Theme.of(context).colorScheme.primary,
-                  trackOutlineColor: WidgetStateProperty.resolveWith<Color?>(
-                    (Set<WidgetState> states) {
-                      if (states.contains(WidgetState.selected)) {
-                        return Theme.of(context).colorScheme.onTertiary;
-                      }
-                      return Theme.of(context).colorScheme.onTertiary;
-                    },
-                  ),
                 ),
               ),
             ),
@@ -170,7 +134,7 @@ class MyDrawer extends StatelessWidget {
             Align(
               alignment: Alignment.bottomCenter,
               child: Padding(
-                padding: const EdgeInsets.only(bottom: 16.0),
+                padding: const EdgeInsets.only(bottom: 12),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
@@ -182,7 +146,7 @@ class MyDrawer extends StatelessWidget {
                             await launchUrl(AppConstants.uriMail);
                           },
                           icon: Icon(
-                            size: 24,
+                            size: styles.iconSmall,
                             MingCuteIcons.mgc_mail_fill,
                             color: Theme.of(context).colorScheme.primary,
                           ),
@@ -192,7 +156,7 @@ class MyDrawer extends StatelessWidget {
                             await launchUrl(AppConstants.uriGithub);
                           },
                           icon: Icon(
-                            size: 24,
+                            size: styles.iconSmall,
                             MingCuteIcons.mgc_github_2_fill,
                             color: Theme.of(context).colorScheme.primary,
                           ),
@@ -202,19 +166,18 @@ class MyDrawer extends StatelessWidget {
                             await launchUrl(AppConstants.uriLinkedin);
                           },
                           icon: Icon(
-                            size: 24,
+                            size: styles.iconSmall,
                             MingCuteIcons.mgc_linkedin_fill,
                             color: Theme.of(context).colorScheme.primary,
                           ),
                         ),
                       ],
                     ),
-                    const SizedBox(height: 8),
                     Text(
                       copyright,
                       style: GoogleFonts.montserrat(
                         color: Theme.of(context).colorScheme.primary,
-                        fontSize: 12,
+                        fontSize: styles.fontMicro,
                       ),
                     ),
                   ],
@@ -224,6 +187,32 @@ class MyDrawer extends StatelessWidget {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _drawerTile({
+    required BuildContext context,
+    required IconData icon,
+    required String title,
+    required VoidCallback onTap,
+    required StyleParams styles,
+    Widget? trailing,
+  }) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: styles.iconSmall,
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      title: Text(
+        title,
+        style: GoogleFonts.montserrat(
+          color: Theme.of(context).colorScheme.primary,
+          fontSize: styles.fontSmall,
+        ),
+      ),
+      onTap: onTap,
+      trailing: trailing,
     );
   }
 }
