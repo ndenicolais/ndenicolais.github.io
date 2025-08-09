088d138de6e91cea78031fe8027d281bc08b4087
ndenicolais <ndn21dev@gmail.com>
2025-08-09 18:49:52 +0200

    Refactor strings.dart to enhance organization and clarity of global strings, including updates

--- lib/utils/strings.dart
+++ lib/utils/strings.dart
@@ -2,116 +2,150 @@
 String me = 'Nicola De Nicolais';
 String copyright = '© 2024 Nicola De Nicolais';
 
-// TOP BAR
-String tTitle = 'My Portfolio';
-String tDrawer = 'NDN';
-String tTop = 'Top';
-String tAbout = 'Bio';
-String tSkills = 'Skills';
-String tWorks = 'Work';
-String tProjects = 'Projects';
-
 // DRAWER
-String dTop = 'Top';
-String dBio = 'Bio';
-String dSkills = 'Skills';
-String dWorks = 'Work';
-String dProjects = 'Projects';
-String dTheme = 'Theme';
+String drawerAboutMe = 'About Me';
+String drawerServices = 'Services';
+String drawerSkillset = 'Skillset';
+String drawerExperiences = 'Experiences';
+String drawerProjects = 'Projects';
+String drawerEducation = 'Education';
+String drawerContacts = 'Contacts';
+String drawerTheme = 'Theme';
 
-// MY TOP
-String job = 'Software Developer';
-String cvLarge = 'Download CV';
-String cvSmall = 'CV';
+// TOP BAR
+String topTitle = 'NDN21';
+String topDrawer = 'NDN21';
+String topAboutMe = 'About Me';
+String topServices = 'Services';
+String topSkillset = 'Skillset';
+String topExperiences = 'Experiences';
+String topProjects = 'Projects';
+String topEducation = 'Education';
+String topContacts = 'Contacts';
 
-// MY BIO
-String sBio = 'My Bio';
-String tBio = 'Bio';
-String description =
-    'I am a software developer with 2 years of experience in research and development. During this time, I started developing Android applications using both the native Kotlin language with XML Layout and the Jetpack Compose framework, as well as the Dart language with the Flutter framework. To design the structures of my apps, I use the tool Figma before translating the designs into code. Currently, I am refining my skills in cross-platform mobile app development with Flutter.';
-String tEducation = 'Education';
-String eDiplomaYear = '2006-2010';
-String eDiplomaType = 'Scientific Diploma';
-String eDiplomaMap = 'Liceo Scientifico G. Rummo of Benevento';
-String eBachelorYear = '2017-2021';
-String eBachelorType = 'Computer Engineering Bachelor';
-String eBachelorMap = 'Università degli Studi Guglielmo Marconi of Roma';
-String tLanguages = 'Languages';
-String lTrinityYear = '2008';
-String lTrinityType = 'B2 English Language Certification';
-String lTrinity = 'Trinity Hall College of Dublin';
-String lGoldsmithYear = '2007';
-String lGoldsmithType = 'B2 English Language Certification';
-String lGoldsmith = 'Goldsmiths College of London';
+// ABOUT ME
+String aboutMeTitle = 'About Me';
+String aboutMeCvLarge = 'Download CV';
+String aboutMeCvSmall = 'CV';
+String aboutMeDes =
+    'I am a software developer specializing in frontend development, with experience in research and development. I have developed Android applications using both native Kotlin with XML layouts and the Jetpack Compose framework, as well as Dart with the Flutter framework. To design both the user flows and the visual aspects of my apps, I use Figma before translating the designs into code. I develop both mobile and web versions of applications, currently focusing on cross-platform solutions with Flutter.';
 
-// MY SKILLS
-String sSkills = 'My Skills';
-String sDart = 'Dart';
-String sFlutter = 'Flutter';
-String sKotlin = 'Kotlin';
-String sCompose = 'Compose';
-String sFigma = 'Figma';
-String sCanva = 'Canva';
-String sVsc = 'VSCode';
-String sStudio = 'Studio';
-String sFirebase = 'Firebase';
-String sMongo = 'MongoDB';
-String sJavascript = 'JavaScript';
-String sTypescript = 'TypeScript';
-String sHTML = 'HTML';
-String sCSS = 'CSS';
-String sReact = 'React';
-String sNode = 'Node.js';
+// SERVICES
+String servicesTitle = 'Services';
+String serviceMobileDev = 'Mobile Development';
+String serviceWebDev = 'Web Development';
+String serviceUiUx = 'UI/UX Design';
+String serviceTechWriting = 'Technical Writing';
+String serviceOpenSource = 'GitHub Open Source';
 
-// MY WORKS
-String sWorks = 'My Works';
-String wArethusaYear = '03/2022-12/2023';
-String wArethusaJob = 'Researcher & Software Engineer';
-String wArethusaMap = 'Arethusa Srl | Casoria (Napoli)';
-String dArethusa = '''
+// SKILLSET
+String skillsetTitle = 'Skillset';
+String skillDart = 'Dart';
+String skillFlutter = 'Flutter';
+String skillKotlin = 'Kotlin';
+String skillCompose = 'Compose';
+String skillFigma = 'Figma';
+String skillCanva = 'Canva';
+String skillVSCode = 'VSCode';
+String skillStudio = 'Studio';
+String skillFirebase = 'Firebase';
+String skillSupabase = 'Supabase';
+String skillMongoDB = 'MongoDB';
+String skillJavascript = 'JavaScript';
+String skillTypescript = 'TypeScript';
+String skillHTML = 'HTML';
+String skillCSS = 'CSS';
+String skillReact = 'React.js';
+String skillNode = 'Node.js';
+
+// EXPERIENCES
+String experiencesTitle = 'Experiences';
+String expGigaServiziYear = '03/2025-Present';
+String expGigaServiziJob = 'Flutter Developer';
+String expGigaServiziCompany = 'Giga Servizi Srl';
+String expGigaServiziLocation = 'Villa di Briano (Caserta)';
+String expGigaServiziDesc = '''
+I work in the IT department of Giga Servizi Srl. I am part of the development team for the creation of CMS software in which I dedicate to the development of the frontend side
+For that project I contributed to:
+• Development of web, desktop and mobile software using different programming languages/platforms/technologies;
+• Collaboration with development and design teams to ensure product quality;
+• Use of GitHub for version control, code reviews, and team collaboration.
+• Requirements analysis and solution design.
+''';
+String expArethusaYear = '03/2022-12/2023';
+String expArethusaJob = 'Researcher & Software Engineer';
+String expArethusaCompany = 'Arethusa Srl';
+String expArethusaLocation = 'Casoria (Napoli)';
+String expArethusaDesc = '''
 I worked in the Research, Development & Innovation (RSI) department of Arethusa Srl. I participated in the implementation of three Research Projects financed by the Campania Region and MISE: ReASSET, DIGGERly and CADS.
-In DIGGERly and CADS projects I participated in the creation of smart Android apps with both Kotlin and Dart languages.
+In the DIGGERly and CADS projects I have mainly been involved in the development of Android applications, using Kotlin (XML layout and Jetpack Compose) and Dart (Flutter).
+In the ReASSET project, I dedicated myself to the integration of blockchain technology, helping to design and implement innovative solutions based on this architecture.
 For all these projects I contributed to:
 • Development of web, desktop and mobile software using different programming languages/platforms/technologies;
 • Collaboration with development and design teams to ensure product quality;
+• Use of GitHub for version control, code reviews, and team collaboration.
 • Requirements analysis and solution design;
 • Drafting of technical documentation.
 ''';
-String tDiggerly = 'DIGGERly';
-String dDiggerly = '''
-DIGERly is a smart app, user friendly and eco-friendly, that allows you to "capture" with a simple smartphone, building assets components and plants, in the time needed for a "walk" to locate the assets of interest in a building.
+String expDiggerlyTitle = 'DIGGERly';
+String expDiggerlyDesc = '''
+A smart APP, user friendly and eco-sustainable, which allows you to “capture” with a simple smartphone, asset components construction and plant engineering, in the necessary space of time of one “walk” to locate assets of interest in a building.
+The main features of DIGGERly are shown below:
+1) Quick Asset Detection & Inventory Assessment: Quick detection of assets and inventory valuation (technical records); digitally tracing the scenarios, the state of affairs, the consistency of real estate assets (architectural, construction, industrial and plant engineering).
+2) Smart Asset Data Collection & Reporting: collection and reporting smart Asset Data (Specialized Knowledge Base for sector); technical and illustrative documentation such as: site inspection certificates, asset mapping, technical records, update and compliance checks.
+3) Asset Performance (Status) - Tracking & Compliance Reporting: periodic monitoring of asset performance (status) e comparison and compliance reporting.
 ''';
-String tReasset = 'ReASSET';
-String dReasset = '''
-ReASSET is a technology platform for the provision of innovative services in the field of Facility Management and digitisation of real estate assets, according to a new organizational model "win-win" that maximizes value and minimises waste.
+String expReassetTitle = 'ReASSET';
+String expReassetDesc = '''
+REASSET project has the aim, as an innovative product-service, of satisfying the increasingly critical need in Italy in the context of Global Maintenance Service services, which is to make maintenance services qualitatively and economically more sustainable of real estate and plant assets.
+The  project concerns the conception, study, design and prototype implementation of a MaaS (Maintenance as a Service) technological platform, as a new product/service based on a collaborative and innovative organizational maintenance model, aimed at eliminating everything that does not produce value in the management of services.
+REASSET platform uses the integrated Blockchain-IoT paradigms for the interoperable and transparent sharing of authenticated and protected data and information, regarding the valorization and real-time operational management of technical-plant assets and technical-economic transactions relating to the interactions between the different subjects involved in the entire Facility Management supply chain.
+The platform involves the development of a cloud web architecture for the development of MaaS (Maintenance as a Service) services, with the integration of Blockchain and IoT technologies and paradigms, the development of Predictive Maintenance processing processes through the acquisition in real time of signals from industrial sensors (e.g. temperature, voltage, current, vibrations, noise, etc.) for the field collection of data on the functionality of Assets.
 ''';
-String tCADS = 'CADS';
-String dCADS = '''
-In CADS a smart app DIGENS was developed for the rapid identification, recording and qualitative-quantitative evaluation of the consistency and danger status of non-structural elements, in their context, in case of seismic events and major stresses.
+String expCadsTitle = 'CADS';
+String expCadsDesc = '''
+In CADS, the DIGENS application was developed with the purpose of creating a tool for rapid detection, recording and qualitative-quantitative evaluation of the state of consistency and danger of non-structural elements (ENS), in their context (location), in the case of seismic events and important stresses.
+The need to be met with DIGENS was to facilitate and increase the productivity of inspections (for the same amount of time) for the detection of the vulnerability of ENS in health areas, so as to encourage as much as possible the restoration of the seismic performances of the ENS themselves.
+The app, through the use of multimedia tools and technologies for capturing reality (photos, films, voice notes, localization, etc.), allows to acquire the data on the seismic classification characteristics of the geographical location (where a property of interest is located) and the intrinsic and installation characteristics (seismic vulnerability) of each of the different ENS that are part of it. The final assessment of the possible level of related danger follows, taking into account, for example, the possible and simultaneous constant presence of people who could be affected by insufficient performance of ENS.
+The app returns the final descriptive and visual report of the ENS detected, ordered by level of seismic danger, estimating the intervention priorities for restoration and performance improvement.
 ''';
-String link = 'Link';
+String expGit = 'Git';
+String expGitHub = 'GitHub';
+String expVSCode = 'Visual Studio Code';
+String expAndroid = 'Android';
+String expFlutter = 'Flutter';
+String expDart = 'Dart';
+String expAndroidStudio = 'Android Studio';
+String expKotlin = 'Kotlin';
+String expCompose = 'Jetpack Compose';
+String expXMLLayout = 'XML Layout';
+String expHyperledger = 'Hyperledger Blockchain';
+String expTypeScript = 'TypeScript';
+String expReact = 'React.js';
+String expNode = 'Node.js';
+String expMongoDB = 'MongoDB';
 
-// MY PROJECTS
-String sProjects = 'My Projects';
+// PROJECTS
+String projectsTitle = 'Projects';
 String tGithub = 'Github';
 String dGithub =
     'All the projects and applications I have developed since the beginning of my career as a software developer are available on GitHub. I started by developing Progressive Web Apps (PWA) using JavaScript and TypeScript, with React.js for the frontend and Node.js with Express.js for the backend. Later, I specialized in developing Android applications using Kotlin, building several apps with traditional XML layouts and later adopting Jetpack Compose to create more modern and dynamic interfaces. I then expanded my skill set by incorporating the Dart language and the Flutter framework, with which I am currently developing applications. This allows me to create high-quality cross-platform solutions, with modern and responsive interfaces, optimizing development with a single codebase for multiple platforms.';
 
 // Flutter
-String pFlutter = 'Flutter';
-String ftitNoteep = 'Noteep';
-String fdesNoteep = 'A digital notebook of tasks.';
-String ftitQRation = 'QRation';
-String fdesQRation = 'A scanner and QR code creator.';
-String ftitCouplers = 'Couplers';
-String fdesCouplers = 'A love events diary.';
-String ftitShox = 'Shox';
-String fdesShox = 'A digital shoes wardrobe.';
-String ftitPortfolio = 'My Portfolio';
-String fdesPortfolio = 'My personal portfolio.';
+String flutterTitle = 'Flutter';
+String flutterShoxTitle = 'Shox';
+String flutterShoxDescription = 'A digital shoes wardrobe.';
+String flutterCouplersTitle = 'Couplers';
+String flutterCouplersDescription = 'A love events diary.';
+String flutterQRationTitle = 'QRation';
+String flutterQRationDescription = 'A QR code scanner & creator.';
+String flutterNoteepTitle = 'Noteep';
+String flutterNoteepDescription = 'A digital notebook of tasks.';
+String flutterPortfolioTitle = 'My Portfolio';
+String flutterPortfolioDescription = 'My personal portfolio.';
 
 // Jetpack Compose
-String pCompose = 'Jetpack Compose';
+String composeTitle = 'Jetpack Compose';
 String composeTitRecorder = 'Recorder';
 String composeDesRecorder =
     'App that allows you to record the input voice through the MediaRecorder class and saves the recording in a file . mp3.';
@@ -147,7 +181,7 @@ String composeDesEgg =
     'App that counts the number of times you click on an egg icon that changes its color each time it is clicked.';
 
 // XML Layouts
-String pXML = 'XML Layouts';
+String xmlTitle = 'XML Layouts';
 String xmlTitSensors = 'Sensors';
 String xmlDesSensors =
     'App that contains a list of functions to detect measurements using the sensors of Android devices.';
@@ -180,7 +214,7 @@ String xmlDesAnimations =
     'List of animations that can be applied to Views, Surfaces, or other objects.';
 
 // React
-String pReact = 'React';
+String reactTitle = 'React';
 String reactTitPWA = 'PWA';
 String reactDesPWA =
     'PWA created with React-based Vite.js with integration of React Router.';
@@ -194,10 +228,37 @@ String reactTitClock = 'Digital Clock';
 String reactDesClock =
     'React-based digital clock and calendar with integration of React Router.';
 
-// FOOTER
-String fHome = 'Benevento, IT';
-String fPhone = '+39 3888605458';
-String fMail = 'n.denicolais@outlook.it';
-String fEmail = 'Mail';
-String fGithub = 'GitHub';
-String fLinkedin = 'LinkedIn';
+// EDUCATION
+String educationTitle = 'Education';
+String tEducationTitle = 'Studies';
+String eBachelorYear = '2017-2021';
+String eBachelorType = 'Computer Engineering Bachelor';
+String eBachelorStructure = 'Università degli Studi Guglielmo Marconi';
+String eBachelorLocation = 'Roma';
+String eDiplomaYear = '2006-2010';
+String eDiplomaType = 'Scientific Diploma';
+String eDiplomaStructure = 'Liceo Scientifico G. Rummo';
+String eDiplomaLocation = 'Benevento';
+String tCertifications = 'Certifications';
+String tLanguages = 'Languages';
+String lTrinityYear = '2008';
+String lTrinityType = 'B2 English Language';
+String lTrinityStructure = 'Trinity Hall College';
+String lTrinityLocation = 'Dublin';
+String lGoldsmithYear = '2007';
+String lGoldsmithType = 'B2 English Language';
+String lGoldsmithStructure = 'Goldsmiths College';
+String lGoldsmithLocation = 'London';
+
+// CONTACTS
+String contactsTitle = 'Contacts';
+String cLocationLabel = 'Location';
+String cLocationValue = 'Benevento, IT';
+String cPhoneLabel = 'Phone';
+String cPhoneValue = '+39 3888605458';
+String cMailLabel = 'Mail';
+String cMailValue = 'n.denicolais@outlook.it';
+String cGithubLabel = 'GitHub';
+String cGithubValue = 'ndenicolais';
+String cLinkedinLabel = 'LinkedIn';
+String cLinkedinValue = 'nicoladenicolais';
