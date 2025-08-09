f22fb46318f17950585d0d474cdaf4707354e8f0
ndenicolais <ndn21dev@gmail.com>
2025-08-09 18:48:47 +0200

    Update color constants in AppColors for consistency and clarity

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -3,9 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color lightYellow = Color(0xFFFFFDF9);
   static const Color darkGrey = Color(0xFF3D3F4D);
-  static const Color lightSand = Color(0xFF979468);
-  static const Color darkYellow = Color(0xFFCCA775);
+  static const Color lightSand = Color(0xFFE7BC91);
+  static const Color darkYellow = Color(0xFFA98467);
   static const Color error = Color(0xFFD23453);
-  static const Color white = Colors.white;
-  static const Color black = Colors.black;
 }
